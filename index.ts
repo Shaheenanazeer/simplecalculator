940de8cb92e28544aa964b955b61440f30ed535f
Shaheena Nazeer <Shaheenanazeer2@gmail.com>
2024-03-30 03:45:44 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter first number", type: "number", name: "firstnumber" },
+  { message: "enter second number", type: "number", name: "secondnumber" },
+  {
+    message: "select one of the operators to perfoam operation",
+    type: "list",
+    name: "operator",
+    choices: ["addition", "subtraction", "multiplication", "division"],
+  },
+]);
+
+console.log(answer);
+
+// conditional statement
+if (answer.operator === "addition") {
+  console.log(answer.firstnumber + answer.secondnumber);
+} else if (answer.operator === "subtraction") {
+  console.log(answer.firstnumber - answer.secondnumber);
+} else if (answer.operator === "multiplication") {
+  console.log(answer.firstnumber * answer.secondnumber);
+} else if (answer.operator === "division") {
+  console.log(answer.firstnumber / answer.secondnumber);
+} else {
+  console.log("please select valid operator");
+}
